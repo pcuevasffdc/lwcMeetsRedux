bd7d93ad4cc1f043abfbec877a846d37e9797fa4
Ana Belén Pelegrina Ortiz <apelegrina@financialforce.com>
2022-03-25 14:55:16 +0100

    ADD app container

---
+++ force-app/main/default/lwc/catTrackAppContainer/catTrackAppContainer.js
@@ -0,0 +1,24 @@
+import { LightningElement, api } from "lwc";
+import { createStore, combineReducers, createLogger } from "c/lwcRedux";
+import reducers from "c/catTrackerReducer";
+
+// Set ENABLE_LOGGING true if you wanna use the logger.
+// We prefer to use the Custom label because we can directly access in the LWC components.
+const ENABLE_LOGGING = true;
+
+export default class TodoAppContainer extends LightningElement {
+	@api store;
+	initialize() {
+		let logger;
+
+		if (ENABLE_LOGGING) {
+			logger = createLogger({
+				duration: true,
+				diff: true
+			});
+		}
+		const combineReducersInstance = combineReducers(reducers);
+		// eslint-disable-next-line @lwc/lwc/no-api-reassignments
+		this.store = createStore(combineReducersInstance, logger);
+	}
+}
