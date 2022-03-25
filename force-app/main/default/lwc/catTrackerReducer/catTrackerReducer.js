712782935782a2e38766dbda1a5ee70fed71c487
Ana Belén Pelegrina Ortiz <apelegrina@financialforce.com>
2022-03-25 14:16:17 +0100

    ADD cat tracker reducer

---
+++ force-app/main/default/lwc/catTrackerReducer/catTrackerReducer.js
@@ -0,0 +1,3 @@
+import { register } from "./registerCat";
+
+export default { register };
---
+++ force-app/main/default/lwc/catTrackerReducer/registerCat.js
@@ -0,0 +1,27 @@
+import { REGISTER_CAT } from "c/catTrackerConstants";
+
+const initialState = {
+	allIds: [],
+	byIds: {}
+};
+
+const register = (state = initialState, action) => {
+	switch (action.type) {
+		case REGISTER_CAT: {
+			const payload = action.payload;
+			console.log(payload);
+			return {
+				...state,
+				allIds: [...state.allIds, payload.id],
+				byIds: {
+					...state.byIds,
+					[payload.id]: { ...payload }
+				}
+			};
+		}
+		default:
+			return state;
+	}
+};
+
+export { register };
