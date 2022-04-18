2fcd61c8cfabe2ad0c4eee94a4e2a5cfcd198266
Ana Belén Pelegrina Ortiz <apelegrina@financialforce.com>
2022-04-18 17:21:36 +0200

    UPDATE CatTracker with call to initialization

--- force-app/main/default/lwc/catTracker/catTracker.js
+++ force-app/main/default/lwc/catTracker/catTracker.js
@@ -1,7 +1,14 @@
 import { LightningElement } from "lwc";
 import { Redux } from "c/lwcRedux";
+import actions from "c/catTrackerActions";
+
 export default class CaTracker extends Redux(LightningElement) {
 	connectedCallback() {
 		super.connectedCallback();
+		this.props.initialize();
+	}
+
+	mapDispatchToProps() {
+		return { initialize: actions.registerCat.initialize };
 	}
 }
