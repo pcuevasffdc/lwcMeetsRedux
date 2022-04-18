fad98614ac727d4832a4131ea2bf6a755bd41b0c
Ana Belén Pelegrina Ortiz <apelegrina@financialforce.com>
2022-04-18 17:20:56 +0200

    ADD action for initial load

--- force-app/main/default/lwc/catTrackerActions/registerCat.js
+++ force-app/main/default/lwc/catTrackerActions/registerCat.js
@@ -1,6 +1,12 @@
-import { REGISTER_CAT, CAT_VACCINATED, CAT_STERILISED } from "c/catTrackerConstants";
+import {
+	REGISTER_CAT,
+	CAT_VACCINATED,
+	CAT_STERILISED,
+	INITIALIZE_APP
+} from "c/catTrackerConstants";
 
 import registerCat from "@salesforce/apex/CatController.registerCat";
+import getAllCats from "@salesforce/apex/CatController.getAllCats";
 
 export const register = (name, gender, age, sterilized, vaccinated) => {
 	return (dispatch) => {
@@ -23,12 +29,27 @@ export const register = (name, gender, age, sterilized, vaccinated) => {
 	};
 };
 
-export const catHasBeenVaccinated = (id)=> ({
+export const catHasBeenVaccinated = (id) => ({
 	type: CAT_VACCINATED,
 	payload: { id }
 });
 
-export const catHasBeenSterilised = (id)=> ({
+export const catHasBeenSterilised = (id) => ({
 	type: CAT_STERILISED,
 	payload: { id }
 });
+
+export const initialize = () => {
+	return (dispatch) => {
+		getAllCats()
+			.then((result) => {
+				dispatch({
+					type: INITIALIZE_APP,
+					payload: JSON.parse(JSON.stringify(result))
+				});
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+	};
+};
