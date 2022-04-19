868f8c31417b6ab3a06aec6819c31afcaacf7504
Paloma de las Cuevas Delgado <pcuevas@financialforce.com>
2022-04-19 22:25:14 +0200

    Adding action and apex method to deregister adopted cats and update the list

--- force-app/main/default/lwc/catTrackerConstants/catTrackerConstants.js
+++ force-app/main/default/lwc/catTrackerConstants/catTrackerConstants.js
@@ -1,5 +1,5 @@
 export const REGISTER_CAT = "REGISTER_CAT";
-export const CAT_IN_ADOPTION = "CAT_IN_ADOPTION";
+export const CAT_ADOPTED = "CAT_ADOPTED";
 export const CAT_STERILISED = "CAT_STERILISED";
 export const CAT_VACCINATED = "CAT_VACCINATED";
 export const INITIALIZE_APP = "INITIALIZE_APP";
