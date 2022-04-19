caed1fad746e75ddd280b5a835e3fe0bae3a12b3
Paloma de las Cuevas Delgado <pcuevas@financialforce.com>
2022-04-19 19:12:17 +0200

    Add button to bring cat to the Vet

--- force-app/main/default/lwc/cat/cat.js
+++ force-app/main/default/lwc/cat/cat.js
@@ -1,5 +1,6 @@
 import { LightningElement, api } from 'lwc';
 import { Redux } from "c/lwcRedux";
+import actions from "c/catTrackerActions";
 
 export default class Cat extends Redux(LightningElement) {
 	@api recordId;
@@ -8,11 +9,31 @@ export default class Cat extends Redux(LightningElement) {
 		return { record : state.catTracker.byIds[this.recordId] };
 	}
 
+	mapDispatchToProps(){
+		return {
+			steriliseCat : actions.registerCat.steriliseCat,
+			vaccinateCat : actions.registerCat.vaccinateCat
+		};
+	}
+
 	get isSterilisedCls() {
 		return this.props.record.sterilized ? "slds-theme_success" : "";
 	}
 
 	get isVaccinatedCls() {
 		return this.props.record.vaccinated ? "slds-theme_success" : "";
+	}
+
+	get catNeedsVet() {
+		return !this.props.record.sterilized || !this.props.record.vaccinated;
+	}
+
+	handleVetVisit() {
+		if (!this.props.record.sterilized) {
+			this.props.steriliseCat(this.recordId);
+		}
+		if (!this.props.record.vaccinated) {
+			this.props.vaccinateCat(this.recordId);
+		}
 	}
 }
