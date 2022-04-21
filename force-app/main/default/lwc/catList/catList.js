27468f39433509941a28d5a224f5b3b4a06c25c7
Ana Belén Pelegrina Ortiz <apelegrina@financialforce.com>
2022-04-21 13:13:13 +0200

    REMOVE CatTracker compoenent and move initializa actiom to catList

--- force-app/main/default/lwc/catList/catList.js
+++ force-app/main/default/lwc/catList/catList.js
@@ -1,12 +1,22 @@
-import { LightningElement } from 'lwc';
+import { LightningElement } from "lwc";
 import { Redux } from "c/lwcRedux";
+import actions from "c/catTrackerActions";
 
 export default class CatList extends Redux(LightningElement) {
+	connectedCallback() {
+		super.connectedCallback();
+		this.props.initialize();
+	}
+
+	mapDispatchToProps() {
+		return { initialize: actions.registerCat.initialize };
+	}
+
 	mapStateToProps(state) {
 		return { allIds: state.catTracker.allIds };
 	}
 
 	get hasRecord() {
-		return this.props.allIds && this.props.allIds.length > 0
+		return this.props.allIds && this.props.allIds.length > 0;
 	}
 }
